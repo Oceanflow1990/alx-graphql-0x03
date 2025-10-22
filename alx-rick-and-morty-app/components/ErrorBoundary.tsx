74645c13805f9ecda00a6f6d2b935745ac9142e0
Opeyemi <eco.ajibade@gmail.com>
2025-10-22 22:34:26 +0100

    Monitor and Log Errors

--- alx-rick-and-morty-app/components/ErrorBoundary.tsx
+++ alx-rick-and-morty-app/components/ErrorBoundary.tsx
@@ -1,3 +1,5 @@
+import * as Sentry from "@sentry/react";
+
 interface State {
   hasError: boolean;
 }
@@ -17,7 +19,7 @@ class ErrorBoundary extends React.Component<ErrorBoundaryProps, State> {
   }
 
   componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
-    console.log({ error, errorInfo });
+    Sentry.captureException(error, { extra: errorInfo });
   }
 
   render() {
